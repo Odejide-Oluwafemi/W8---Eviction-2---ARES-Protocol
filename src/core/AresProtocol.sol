eafa3910536650307af837239e944339c788f508
LordDregg <oluwatosinodejide121@gmail.com>
2026-03-10 19:40:40 +0100

    more on proposal manager

--- src/core/AresProtocol.sol
+++ src/core/AresProtocol.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract AresProtocol {}
+import {AresToken} from "src/core/AresToken.sol";
+
+contract AresProtocol {
+    AresToken immutable token;
+
+    constructor(address aresToken) {
+        token = AresToken(aresToken);
+    }
+}
--- src/modules/ProposalManager.sol
+++ src/modules/ProposalManager.sol
@@ -2,8 +2,21 @@
 pragma solidity ^0.8.24;
 
 contract ProposalManager {
-    // events
+    // Errors
+    error ProposalManager__CannotExecuteTransaction();
+    error ProposalManager__DuplicateProposal();
+    error ProposalManager__InvalidSigner();
+    error ProposalManager__CannotCommitOwnProposal();
+
+    // Events
     event ProposalCreated(bytes32 id, address proposer);
+
+    bytes public constant TRANSFER_ACTION = abi.encodeWithSignature("transfer(address,address,uint256)");
+    bytes public constant CALL_ACTION = abi.encodeWithSignature("call(address,bytes)");
+    bytes public constant UPGRADE_ACTION = abi.encodeWithSignature("upgrade(address)");
+
+    address[] public signers;
+    uint8 public quoremCount;
 
     struct Proposal {
         address proposer;
@@ -14,15 +27,47 @@ contract ProposalManager {
         uint256 timestamp;
         bool approved;
         bool cancelled;
+        ProposalStatus status;
+    }
+
+    enum ProposalStatus {
+        Draft, // Newly Created
+        Commit, // Quorum Achieved
+        Queued, // In TimeLock Period
+        ReadyForExecution, // Can be Executed By Proposer
+        Executed // Has been executed
     }
 
     mapping(bytes32 => Proposal) public proposals;
     mapping(address => uint256) public nonces;
+    mapping(address signer => bool valid) private isValidSigner;
 
-    function createProposal(address target, uint256 value, bytes calldata data) external returns (bytes32 id) {
+    constructor(address[] memory _signers, uint8 _quoremCount) {
+        quoremCount = _quoremCount;
+
+        for (uint256 i; i < _signers.length; i++) {
+            signers[i] = _signers[i];
+            isValidSigner[_signers[i]] = true;
+        }
+    }
+
+    // Modifiers
+    modifier onlyValidSigner() {
+        if (!isValidSigner[msg.sender]) revert ProposalManager__InvalidSigner();
+
+        _;
+    }
+
+    function createProposal(address target, uint256 value, bytes calldata data)
+        external
+        onlyValidSigner
+        returns (bytes32 id)
+    {
         uint256 nonce = nonces[msg.sender]++;
 
         id = keccak256(abi.encode(msg.sender, target, value, data, nonce));
+
+        if (proposals[id].proposer != address(0)) revert ProposalManager__DuplicateProposal();
 
         proposals[id] = Proposal({
             proposer: msg.sender,
@@ -32,7 +77,25 @@ contract ProposalManager {
             nonce: nonce,
             timestamp: block.timestamp,
             approved: false,
-            cancelled: false
+            cancelled: false,
+            status: ProposalStatus.Draft
         });
+    }
+
+    function commitProposal(bytes32 id) external onlyValidSigner {
+        Proposal storage proposal = proposals[id];
+        if (msg.sender == proposal.proposer) revert ProposalManager__CannotCommitOwnProposal();
+        proposal.status = ProposalStatus.Queued;
+    }
+
+    function execute(Proposal memory proposal) external returns (bool, bytes memory) {
+        if (proposal.status == ProposalStatus.ReadyForExecution) revert ProposalManager__CannotExecuteTransaction();
+        (bool success, bytes memory data) = proposal.target.call{value: proposal.value}(proposal.data);
+
+        return (success, data);
+    }
+
+    function getProposalById(bytes32 id) external view returns (Proposal memory) {
+        return proposals[id];
     }
 }
